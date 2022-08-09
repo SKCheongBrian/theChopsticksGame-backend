d2ca6b06b26eae19acd6bd84cb3efb95bb1f3dbf
skcheongbrian <skcheongbrian@gmail.com>
2022-08-09 12:55:07 +0800

    migrate to desktop

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { v4: uuid } = require("uuid");
+
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const webSocketServer = new WebSocket.Server({ server: server });
+
+// clients that are connected
+const clients = {};
+
 app.use(bodyParser.json());
 
 app.use("/api/users", usersRoutes);
@@ -22,4 +32,35 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error has occurred!" });
 });
 
-app.listen(4000);
+webSocketServer.on("request", function (request) {
+  var userId = uuid();
+  console.log(
+    new Date() +
+      " Received a new connection from origin " +
+      request.origin +
+      "."
+  );
+
+  const connection = request.accept(null, request.origin);
+
+  clients[userId] = {
+    connection: connection,
+  };
+  console.log(
+    "connected " + userId + " in " + Object.getOwnPropertyNames(clients)
+  );
+
+  connection.on("message", (message) => {
+    const result = JSON.parse(message.utf8Data);
+    console.log(result);
+  });
+
+  const payLoad = {
+    method: "connect",
+    clientId: userId,
+  };
+
+  connection.send(JSON.stringify(payLoad));
+});
+
+server.listen(4000, () => console.log("Listening on port: 4000"));
